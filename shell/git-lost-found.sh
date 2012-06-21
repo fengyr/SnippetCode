92a96853f2c5269754cf49e0eb7fbb11e43dd8cf
Zenki.J.Zha <Zenki.J.Zha@foxconn.com>
2012-06-21 17:25:27 +0800

    update git-lost-found.sh

--- shell/git-lost-found.sh
+++ shell/git-lost-found.sh
@@ -18,16 +18,16 @@ rm -fr "$laf" && mkdir -p "$laf/commit" "$laf/other" || exit
 git fsck --full --no-reflogs |
 while read dangling type sha1
 do
-        case "$dangling" in
-        dangling)
-                if git rev-parse -q --verify "$sha1^0" >/dev/null
-                then
-                        dir="$laf/commit"
-                        git show-branch "$sha1"
-                else
-                        dir="$laf/other"
-                fi
-                echo "$sha1" >"$dir/$sha1"
-                ;;
-        esac
+    case "$dangling" in
+    dangling)
+        if git rev-parse -q --verify "$sha1^0" >/dev/null
+        then
+            dir="$laf/commit"
+            git show-branch "$sha1"
+        else
+            dir="$laf/other"
+        fi
+        echo "$sha1" >"$dir/$sha1"
+        ;;
+    esac
 done
