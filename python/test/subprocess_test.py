b4f3a7be1b31df89f7a0f74effc96128aad4fb4c
junzha <junzha@pateo.com.cn>
2013-03-29 17:57:26 +0800

    Force commit by sync_github.sh
    Change-Id: I12b30b4c2f65c4cd5684acc542bbeaf3dce9b613

--- python/test/subprocess_test.py
+++ python/test/subprocess_test.py
@@ -5,17 +5,17 @@ import subprocess
 import thread
 import time
 
-cmd = ['top', '-d', '0.5']
+cmd = ['top', '-d', '0.5', '-n', '3']
 
 #subprocess.Popen(cmd, shell=True)
 proc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
 
-def message(*arg):
+def filterMessage(*arg):
     while True:
         time.sleep(0.5)
         p = subprocess.Popen(['grep', 'Cpu'], stdin=arg[0])
 
-thread.start_new_thread(message, (proc.stdout, 0))
+thread.start_new_thread(filterMessage, (proc.stdout, 0))
 
 while True:
     time.sleep(1)
