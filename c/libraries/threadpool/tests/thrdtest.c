f166e9f892f5004bcf396b00671ba951429a3761
Zenki.J.Zha <zenki2001cn@163.com>
2014-10-26 22:21:00 +0800

    Add threadpool

---
+++ c/libraries/threadpool/tests/thrdtest.c
@@ -0,0 +1,48 @@
+#define THREAD 4
+#define QUEUE  256
+
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <assert.h>
+
+#include "threadpool.h"
+
+int tasks = 0, done = 0;
+pthread_mutex_t lock;
+
+void dummy_task(void *arg) {
+    tasks++;
+    printf("%d\n", tasks);
+    sleep(3);
+    /* pthread_mutex_lock(&lock); */
+    /* done++; */
+    /* pthread_mutex_unlock(&lock); */
+}
+
+int main(int argc, char **argv)
+{
+    threadpool_t *pool;
+
+    pthread_mutex_init(&lock, NULL);
+
+    assert((pool = threadpool_create(THREAD, QUEUE, 0)) != NULL);
+    fprintf(stderr, "Pool started with %d threads and "
+            "queue size of %d\n", THREAD, QUEUE);
+
+    while(threadpool_add(pool, &dummy_task, NULL, 0) == 0) {
+        /* pthread_mutex_lock(&lock); */
+        /* tasks++; */
+        /* pthread_mutex_unlock(&lock); */
+    }
+
+    /* fprintf(stderr, "Added %d tasks\n", tasks); */
+
+    while((tasks) > done) {
+        usleep(10000);
+    }
+    assert(threadpool_destroy(pool, 0) == 0);
+    /* fprintf(stderr, "Did %d tasks\n", done); */
+
+    return 0;
+}
